18c342699902591645bbf2e3cb1f0da9698524c1
Chris Massengale <chris.massengale@verizon.net>
2018-10-02 13:38:16 -0400

    added a sockets io call to allow 2way communication to/from browser and server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 require('./config/config');
 
-
+const http = require('http');
 const express = require('express');
 const path = require('path');  //Don't need to install , part of nodejs
+const socketIO = require('socket.io');
+
 
 const publicPath = path.join(__dirname,"..", "public");
 
 const port = process.env.PORT;
 
 let app = express();
+let server = http.createServer(app);
 
-// app.use(express.static(publicPath))
+let io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New User Connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected!')
+  });
+}); //turns on
+// app.use(express.static(publicPath)) //Used in video to display the index
 
 app.get('/', (request, response) => {
   response.status(200).sendFile(publicPath + "/index.html");
@@ -18,7 +30,7 @@ app.get('/', (request, response) => {
 
 
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up and running on Port: ${port}`)
 });
 
